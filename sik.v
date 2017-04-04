d0ad36b9e94f017df7af4c6f5303b8562ba0d3e4
Justin Hill <justinwhill94@gmail.com>
2017-04-04 18:59:31 -0400

    Added Sizes and Opcodes to sik.v. Added example sik.aik file

--- sik.v
+++ sik.v
@@ -1,3 +1,51 @@
+//#######################################################################
+// Initialize Constants with `define
+//#######################################################################
+
+// Standard Sizes
+`define WORD	[15:0]
+`define Opcode	[15:12]
+`define Immed	[11:0]
+`define STATE	[7:0]
+`define PRE	[3:0]
+`define REGSIZE	[255:0]
+`define REGNUM	[7:0]
+`define MEMSIZE [65535:0]
+
+// Opcode State Numbers
+`define OPadd 
+`define OPand 
+`define OPcall 
+`define OPdup 
+`define OPget 
+`define OPjumpf 
+`define OPjump 
+`define OPjumpt 
+`define OPload 
+`define OPlt 
+`define OPor 
+`define OPpop 
+`define OPpre 
+`define OPpush 
+`define OPput 
+`define OPret 
+`define OPstore 
+`define OPsub 
+`define OPsys 
+`define OPtest 
+`define OPxor 
+`define OPstart 
+`define OPstart1 
+
+//#######################################################################
+// Main Processor Module
+//#######################################################################
+
+
+
+//#######################################################################
+// Test Bench
+//#######################################################################
 module testbench;
 reg reset = 0;
 reg clk = 0;
